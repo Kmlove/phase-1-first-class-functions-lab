a8e801f92c2d3881d8a53b102a480f7a08ea1079
Kim Love <kimberlymlove15@gmail.com>
2023-08-01 15:46:17 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (namesArray) {
+  let firstTwoNames = namesArray.slice(0, 2);
+  return firstTwoNames;
+};
+
+const returnLastTwoDrivers = function (namesArray) {
+  let lastTwoNames = namesArray.slice(-2);
+  return lastTwoNames;
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function(fare){
+        return fare * integer;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(namesArray, cb){
+    return cb(namesArray);
+}
